f7f8f07bd811a9e914fb5f2b1bb33c823ef03b58
copypasteearth <copypasteearth@gmail.com>
2017-07-18 11:34:00 -0400

    json addition folder structure changes

---
+++ JSON/jsonobject.php
@@ -0,0 +1,15 @@
+<?php
+$myObj = new \stdClass();
+$myObj->name = "John";
+$myObj->age = 30;
+$myObj->city = "New York";
+$myArray = array();
+for($i = 0;$i < 10;$i++){
+    $myArray[$i] = "hello world" . $i;
+}
+$myObj->hello = $myArray;
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
